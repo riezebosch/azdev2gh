3222b1bbae1514411f88103a734d680c87cd4602
Anne Meijer <anne@MacBook-Pro-3.local>
2020-02-07 13:52:47 +0100

    chore: removed custom rest implementation

--- Requests.Tests/WorkItemTest.cs
+++ Requests.Tests/WorkItemTest.cs
@@ -1,25 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using AzureDevOpsRest.Tests;
+using FluentAssertions;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
+using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
+using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.Services.WebApi;
+
 using Xunit;
 
 namespace AzureDevOpsRest.Requests.Tests
 {
     public class WorkItemTest
     {
+        private string _project = "github-migration";
+        private readonly TestConfig _config = new TestConfig();
+
         [Fact]
         public async Task List()
         {
             var client = new Client("manuel");
-            var project = "github-migration";
-            
-            await client.GetAsync(new Request<WorkItem>($"{project}/_apis/wit/workitems", "5.1").WithQueryParams(("ids", "1434")));
-            
+
+            await client.GetAsync(new Request<WorkItem>($"{_project}/_apis/wit/workitems", "5.1").WithQueryParams(("ids", "1434")));
         }
 
         [Fact]
         public async Task ClientList()
         {
-            var client = VssConnection()
+            var connection = new VssConnection(new Uri($"https://dev.azure.com/manuel"), new VssBasicCredential("", ""));
+            var client = connection.GetClient<WorkItemTrackingHttpClient>();
+            
+            var result = await client.GetWorkItemsAsync(_project, new List<int> {1434});
+
+            result.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task GetWorkItemIds()
+        {
+            var connection = new VssConnection(new Uri($"https://dev.azure.com/{_config.Organization}"), new VssBasicCredential("",_config.Token));
+            var client = connection.GetClient<WorkItemTrackingHttpClient>();
+
+            var wiql = new Wiql()
+            {
+               Query = "Select [Id], [State], [Title] " +
+                "From WorkItems"
+                
+            };
+            var result = await client.QueryByWiqlAsync(wiql);
+
+            result.Should().NotBeNull();
+            
+            
         }
     }
 

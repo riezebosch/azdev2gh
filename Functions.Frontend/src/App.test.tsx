eec6480b6140f1a291720cd249f5887f86062e3f
Manuel Riezebosch <mriezebosch@gmail.com>
2020-02-24 16:10:34 +0100

    fix(html): use prettier

--- Functions.Frontend/src/App.test.tsx
+++ Functions.Frontend/src/App.test.tsx
@@ -1,107 +1,157 @@
-import React from 'react';
-import { render, fireEvent, wait, screen, waitForElementToBeRemoved } from '@testing-library/react';
-import App from './App';
-import axios from 'axios'
+import React from "react";
+import {
+  render,
+  fireEvent,
+  wait,
+  waitForElementToBeRemoved
+} from "@testing-library/react";
+import App from "./App";
+import axios from "axios";
 
-jest.mock('axios')
+jest.mock("axios");
 const axiosMock = axios as jest.Mocked<typeof axios>;
 
-describe('app', () => {
-  it('invokes api on button click', async () => {
-    const { getByLabelText, getByRole } = render(<App />);  
-    
-    axiosMock.post.mockResolvedValueOnce({
-      data: { greeting: 'hello there' },
-    })
-    
-    fireEvent.change(getByLabelText('azure devops token *'), { target: { value: 'xxx' }});
-    fireEvent.change(getByLabelText('organization *'), { target: { value: 'test' }});
-    fireEvent.change(getByLabelText('area path *'), { target: { value: 'test/team A' }});
-    fireEvent.change(getByLabelText('github token *'), { target: { value: 'yyy' }});
-    fireEvent.click(getByRole('button'));
+describe("app", () => {
+  it("invokes api on button click", async () => {
+    const { getByLabelText, getByRole } = render(<App />);
 
-    await wait(() => getByRole('button'));
+    axiosMock.post.mockResolvedValueOnce({
+      data: { greeting: "hello there" }
+    });
+
+    fireEvent.change(getByLabelText("azure devops token *"), {
+      target: { value: "xxx" }
+    });
+    fireEvent.change(getByLabelText("organization *"), {
+      target: { value: "test" }
+    });
+    fireEvent.change(getByLabelText("area path *"), {
+      target: { value: "test/team A" }
+    });
+    fireEvent.change(getByLabelText("github token *"), {
+      target: { value: "yyy" }
+    });
+    fireEvent.click(getByRole("button"));
+
+    await wait(() => getByRole("button"));
     expect(axiosMock.post).toHaveBeenCalledWith(expect.any(String), {
       azureDevOps: {
-        token: 'xxx',
-        organization: 'test',
-        areaPath: 'test/team A'
+        token: "xxx",
+        organization: "test",
+        areaPath: "test/team A"
       },
       github: {
-        token: 'yyy'
+        token: "yyy"
       }
     });
   });
 
-  it('shows snackbar on post error', async () => {
-    const { getByLabelText, getByRole, getByText } = render(<App />);  
-    
-    axiosMock.post.mockRejectedValue('something wrong');
+  it("shows snackbar on post error", async () => {
+    const { getByLabelText, getByRole, getByText } = render(<App />);
 
-    fireEvent.change(getByLabelText('azure devops token *'), { target: { value: 'xxx' }});
-    fireEvent.change(getByLabelText('organization *'), { target: { value: 'test' }});
-    fireEvent.change(getByLabelText('area path *'), { target: { value: 'test/team A' }});
-    fireEvent.change(getByLabelText('github token *'), { target: { value: 'yyy' }});
-    fireEvent.click(getByRole('button'));
+    axiosMock.post.mockRejectedValue("something wrong");
 
-    await wait(() => getByText('something wrong'));
+    fireEvent.change(getByLabelText("azure devops token *"), {
+      target: { value: "xxx" }
+    });
+    fireEvent.change(getByLabelText("organization *"), {
+      target: { value: "test" }
+    });
+    fireEvent.change(getByLabelText("area path *"), {
+      target: { value: "test/team A" }
+    });
+    fireEvent.change(getByLabelText("github token *"), {
+      target: { value: "yyy" }
+    });
+    fireEvent.click(getByRole("button"));
+
+    await wait(() => getByText("something wrong"));
   });
 
-  it('shows function error when runtime status is failed', async () => {
-    const { getByLabelText, getByRole, getByText } = render(<App />);  
-    
-    axiosMock.post.mockResolvedValueOnce({ 
+  it("shows function error when runtime status is failed", async () => {
+    const { getByLabelText, getByRole, getByText } = render(<App />);
+
+    axiosMock.post.mockResolvedValueOnce({
       data: {
-        "name":"Migrate",
-        "instanceId":"0535fdb8b11b45f08f91b01975cd905d",
-        "runtimeStatus":"Failed",
-        "input":{"AzureDevOps":{"Organization":"","Token":"","AreaPath":""},"GitHub":{"Token":""}},
-        "customStatus":null,
-        "output":"Orchestrator function 'Migrate' failed: some error"
+        name: "Migrate",
+        instanceId: "0535fdb8b11b45f08f91b01975cd905d",
+        runtimeStatus: "Failed",
+        input: {
+          AzureDevOps: { Organization: "", Token: "", AreaPath: "" },
+          GitHub: { Token: "" }
+        },
+        customStatus: null,
+        output: "Orchestrator function 'Migrate' failed: some error"
       }
     });
 
-    fireEvent.change(getByLabelText('azure devops token *'), { target: { value: 'xxx' }});
-    fireEvent.change(getByLabelText('organization *'), { target: { value: 'test' }});
-    fireEvent.change(getByLabelText('area path *'), { target: { value: 'test/team A' }});
-    fireEvent.change(getByLabelText('github token *'), { target: { value: 'yyy' }});
-    fireEvent.click(getByRole('button'));
+    fireEvent.change(getByLabelText("azure devops token *"), {
+      target: { value: "xxx" }
+    });
+    fireEvent.change(getByLabelText("organization *"), {
+      target: { value: "test" }
+    });
+    fireEvent.change(getByLabelText("area path *"), {
+      target: { value: "test/team A" }
+    });
+    fireEvent.change(getByLabelText("github token *"), {
+      target: { value: "yyy" }
+    });
+    fireEvent.click(getByRole("button"));
 
-    await wait(() => getByText('function failed'));
+    await wait(() => getByText("function failed"));
   });
 
-  it('shows loading component while awaiting the api response', async () => {
-    const { getByLabelText, getByRole, getByTestId, queryByTestId } = render(<App />);  
+  it("shows loading component while awaiting the api response", async () => {
+    const { getByLabelText, getByRole, getByTestId, queryByTestId } = render(
+      <App />
+    );
 
-    axiosMock.post.mockResolvedValue({ 
+    axiosMock.post.mockResolvedValue({
       data: null
     });
 
-    fireEvent.change(getByLabelText('azure devops token *'), { target: { value: 'xxx' }});
-    fireEvent.change(getByLabelText('organization *'), { target: { value: 'test' }});
-    fireEvent.change(getByLabelText('area path *'), { target: { value: 'test/team A' }});
-    fireEvent.change(getByLabelText('github token *'), { target: { value: 'yyy' }});
-    fireEvent.click(getByRole('button'));
+    fireEvent.change(getByLabelText("azure devops token *"), {
+      target: { value: "xxx" }
+    });
+    fireEvent.change(getByLabelText("organization *"), {
+      target: { value: "test" }
+    });
+    fireEvent.change(getByLabelText("area path *"), {
+      target: { value: "test/team A" }
+    });
+    fireEvent.change(getByLabelText("github token *"), {
+      target: { value: "yyy" }
+    });
+    fireEvent.click(getByRole("button"));
 
-    await wait(() => getByTestId('loading'));
-    expect(queryByTestId('error')).toBe(null);  
+    await wait(() => getByTestId("loading"));
+    expect(queryByTestId("error")).toBe(null);
 
-    await waitForElementToBeRemoved(() => getByTestId('loading'));
+    await waitForElementToBeRemoved(() => getByTestId("loading"));
   });
 
-  it('disables go button until all required fields have value', async () => {
-    const { getByLabelText, getByRole } = render(<App />);  
-    
-    fireEvent.change(getByLabelText('azure devops token *'), { target: { value: 'xxx' }});
-    expect(getByRole('button')).toHaveAttribute("disabled");
+  it("disables go button until all required fields have value", async () => {
+    const { getByLabelText, getByRole } = render(<App />);
 
-    fireEvent.change(getByLabelText('organization *'), { target: { value: 'test' }});
-    expect(getByRole('button')).toHaveAttribute("disabled");
+    fireEvent.change(getByLabelText("azure devops token *"), {
+      target: { value: "xxx" }
+    });
+    expect(getByRole("button")).toHaveAttribute("disabled");
 
-    fireEvent.change(getByLabelText('area path *'), { target: { value: 'test/team A' }});
-    expect(getByRole('button')).toHaveAttribute("disabled");
+    fireEvent.change(getByLabelText("organization *"), {
+      target: { value: "test" }
+    });
+    expect(getByRole("button")).toHaveAttribute("disabled");
 
-    fireEvent.change(getByLabelText('github token *'), { target: { value: 'yyy' }});
-    expect(getByRole('button')).not.toHaveAttribute("disabled");
+    fireEvent.change(getByLabelText("area path *"), {
+      target: { value: "test/team A" }
+    });
+    expect(getByRole("button")).toHaveAttribute("disabled");
+
+    fireEvent.change(getByLabelText("github token *"), {
+      target: { value: "yyy" }
+    });
+    expect(getByRole("button")).not.toHaveAttribute("disabled");
   });
 });
--- Functions.Frontend/src/App.tsx
+++ Functions.Frontend/src/App.tsx
@@ -1,80 +1,124 @@
-import React, { useState } from 'react';
-import './App.css';
-import axios from 'axios';
-import { Button, TextField, Card, Snackbar, CircularProgress, CardContent, Box } from '@material-ui/core'
-import { Alert } from '@material-ui/lab'
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import {
+  Button,
+  TextField,
+  Card,
+  Snackbar,
+  CircularProgress,
+  CardContent,
+  Box
+} from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 
 function App() {
-  const [azureDevopsToken, setAzureDevopsToken] = useState('');
-  const [azureDevOpsOrganization, setAzureDevOpsOrganization] = useState('');
-  const [azureDevOpsAreaPath, setAzureDevOpsAreaPath] = useState('');
-  const [gitHubToken, setGitHubToken] = useState('');
+  const [azureDevopsToken, setAzureDevopsToken] = useState("");
+  const [azureDevOpsOrganization, setAzureDevOpsOrganization] = useState("");
+  const [azureDevOpsAreaPath, setAzureDevOpsAreaPath] = useState("");
+  const [gitHubToken, setGitHubToken] = useState("");
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   return (
     <div className="App">
       <header className="App-header">
-        <Box width={1/2}>
-          <Card >
-            <CardContent >
-              <form onSubmit={e => {
-                setLoading(true);
-                axios.post('https://azdo2gh.azurewebsites.net/api/migrate', { 
-                  azureDevOps: {
-                    token: azureDevopsToken,
-                    organization: azureDevOpsOrganization,
-                    areaPath: azureDevOpsAreaPath
-                  },
-                  github: {
-                    token: gitHubToken
-                  }
-                })
-                .then(r => r.data?.runtimeStatus === 'Failed' ? setError('function failed') : setError(''))
-                .catch(x => setError(x.toString()))
-                .finally(() => setLoading(false));
+        <Box width={1 / 2}>
+          <Card>
+            <CardContent>
+              <form
+                onSubmit={e => {
+                  setLoading(true);
+                  axios
+                    .post("https://azdo2gh.azurewebsites.net/api/migrate", {
+                      azureDevOps: {
+                        token: azureDevopsToken,
+                        organization: azureDevOpsOrganization,
+                        areaPath: azureDevOpsAreaPath
+                      },
+                      github: {
+                        token: gitHubToken
+                      }
+                    })
+                    .then(r =>
+                      r.data?.runtimeStatus === "Failed"
+                        ? setError("function failed")
+                        : setError("")
+                    )
+                    .catch(x => setError(x.toString()))
+                    .finally(() => setLoading(false));
 
-                e.preventDefault(); // prevent from onSubmit errors in tests
-              }}>
-                <TextField 
-                  label="azure devops token" 
+                  e.preventDefault(); // prevent from onSubmit errors in tests
+                }}
+              >
+                <TextField
+                  label="azure devops token"
                   id="azure-devops-token"
                   value={azureDevopsToken}
-                  onChange={x => setAzureDevopsToken(x.currentTarget.value) }
+                  onChange={x => setAzureDevopsToken(x.currentTarget.value)}
                   required={true}
                   fullWidth
-                  helperText="scope: Work Item (Read)" />
-                  <TextField 
-                    label="organization"
-                    id="azure-devops-organization"
-                    value={azureDevOpsOrganization}
-                    required={true}
-                    fullWidth
-                    onChange={x => setAzureDevOpsOrganization(x.currentTarget.value) } />
-                  <TextField
-                    label="area path"
-                    id="azure-devops-areapath"
-                    value={azureDevOpsAreaPath}
-                    required={true}
-                    fullWidth
-                    onChange={x => setAzureDevOpsAreaPath(x.currentTarget.value) } />
-                  <TextField 
-                    label="github token" 
-                    id="github-token"
-                    value={gitHubToken}
-                    required={true}
-                    fullWidth
-                    onChange={x => setGitHubToken(x.currentTarget.value) }
-                    helperText="scope: public_repo" />
-                <Button disabled={ loading || [azureDevopsToken, azureDevOpsOrganization, azureDevOpsAreaPath, gitHubToken].includes('') } type="submit">Go!</Button>    
+                  helperText="scope: Work Item (Read)"
+                />
+                <TextField
+                  label="organization"
+                  id="azure-devops-organization"
+                  value={azureDevOpsOrganization}
+                  required={true}
+                  fullWidth
+                  onChange={x =>
+                    setAzureDevOpsOrganization(x.currentTarget.value)
+                  }
+                />
+                <TextField
+                  label="area path"
+                  id="azure-devops-areapath"
+                  value={azureDevOpsAreaPath}
+                  required={true}
+                  fullWidth
+                  onChange={x => setAzureDevOpsAreaPath(x.currentTarget.value)}
+                />
+                <TextField
+                  label="github token"
+                  id="github-token"
+                  value={gitHubToken}
+                  required={true}
+                  fullWidth
+                  onChange={x => setGitHubToken(x.currentTarget.value)}
+                  helperText="scope: public_repo"
+                />
+                <Button
+                  disabled={
+                    loading ||
+                    [
+                      azureDevopsToken,
+                      azureDevOpsOrganization,
+                      azureDevOpsAreaPath,
+                      gitHubToken
+                    ].includes("")
+                  }
+                  type="submit"
+                >
+                  Go!
+                </Button>
               </form>
             </CardContent>
           </Card>
         </Box>
-        <Snackbar open={error !== ''} autoHideDuration={6000} onClose={ () => setError('') } data-testid="error">
-            <Alert severity='error'>{error}</Alert>
+        <Snackbar
+          open={error !== ""}
+          autoHideDuration={6000}
+          onClose={() => setError("")}
+          data-testid="error"
+        >
+          <Alert severity="error">{error}</Alert>
         </Snackbar>
-        <Snackbar open={loading} autoHideDuration={6000} onClose={ () => setLoading(false) }  data-testid="loading">
+        <Snackbar
+          open={loading}
+          autoHideDuration={6000}
+          onClose={() => setLoading(false)}
+          data-testid="loading"
+        >
           <CircularProgress />
         </Snackbar>
       </header>
